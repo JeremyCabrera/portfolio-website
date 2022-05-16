68702df877e6e6ff8d0141124037fe4206277e99
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2022-05-16 10:46:01 -0400

    Added grid for projects links

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        I'm Jeremy, your local<br />
+        FullStack Guerilla!
+      </SectionTitle>
+      <SectionText>
+        Below you will find a few Full Stack JavaScript Applications I have built, deployed and currently operate. &nbsp;If you're looking for a Full-Stack Engineer for any of your project needs please reach out!
+      </SectionText>
+      <Button onClick={() => window.location = "https://linkedin.com/in/jeremy-cabrera-4044a8179/"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
