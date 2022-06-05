104023a27a1d05b430b7a303a6ddfdc9cd34bcbe
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2022-06-05 14:30:20 -0400

    Portfolio init

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,11 +8,11 @@ const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        I'm Jeremy, your local<br />
-        FullStack Guerilla!
+        I'm Jeremy, a FullStack<br />
+        JavaScript Developer!
       </SectionTitle>
       <SectionText>
-        Below you will find a few Full Stack JavaScript Applications I have built, deployed and currently operate. &nbsp;If you're looking for a Full-Stack Engineer for any of your project needs please reach out!
+        Below you will find a few FullStack JavaScript Applications I have built, deployed and currently manage. &nbsp;If you're looking for a Full-Stack Engineer for any of your project needs, please reach out!
       </SectionText>
       <Button onClick={() => window.location = "https://linkedin.com/in/jeremy-cabrera-4044a8179/"}>Learn More</Button>
     </LeftSection>
